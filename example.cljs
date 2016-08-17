e6558c6a5915c1a229037a27bc66eecb42aaf0e1
nathants <me@nathants.com>
2016-08-16 23:38:17 -0700

    iterating

--- example.cljs
+++ example.cljs
@@ -1,14 +1,28 @@
-(require-npm "express" "4.14.0")
-(require-lein "prismatic/schema" "1.1.3")
-(require [clojure.pprint :as pprint])
+(ns main
+  #_(:npm [express "4.14.0"])
+  #_(:lein [prismatic/schema "1.1.3"])
+  (:require [clojure.pprint :as pprint]))
 
-(def express (js/require "express"))
-(def app (express))
+(def args (drop 2 (.-argv js/process)) )
 
-(.use app (fn [req res next]
-            (.on res "finish" #(println (.-statusCode res) (.-url req)))
-            (next)))
-(.get app "/status" (fn [req res]
-                      (.send res "200 a ok!")))
-(println "listen on 8000")
-(.listen app 8000)
+(def cli-mode (not= 1 (count (.-argv js/process))))
+
+(defn logging-middleware
+  [req res next]
+  (.on res "finish" #(println (.-statusCode res) (.-url req)))
+  (next))
+
+(defn status-handler
+  [req res]
+  (.send res "200 a ok!!!?????"))
+
+(defn start-app [port]
+  (println "listening on:" port)
+  (doto ((js/require "express"))
+    (.use #(apply #'logging-middleware %&))
+    (.get "/status" #(apply #'status-handler %&))
+    (.listen port)))
+
+(when cli-mode
+  (let [[port] args]
+    (start-app (js/parseInt port))))
