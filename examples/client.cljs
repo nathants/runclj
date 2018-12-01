c72b646cf4e2b80cdc7f9a04083aad3ae27e6555
nathants <me@nathants.com>
2018-11-30 23:01:50 -0800

    bugfix client

--- examples/client.cljs
+++ examples/client.cljs
@@ -55,7 +55,7 @@
 
   (defn -main []
     (defonce repl (repl/connect "http://localhost:9000/repl"))
-    (defonce router (bide/start! router {:default home :on-navigate #(swap! state assoc :page %) :html5? false}))
+    (bide/start! router {:default home :on-navigate #(swap! state assoc :page %) :html5? false})
     (reagent/render [root] (js/document.getElementById "app")))
 
   (-main) ;; trigger react for browser repl workflow
