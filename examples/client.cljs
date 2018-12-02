7472ae602b4c2658ba4ab84add1faa24411e8159
nathants <me@nathants.com>
2018-12-02 00:11:14 -0800

    notes on mobile repl

--- examples/client.cljs
+++ examples/client.cljs
@@ -54,7 +54,7 @@
       ["(.*)" not-found]]))
 
   (defn -main []
-    (defonce repl (repl/connect "http://localhost:9000/repl"))
+    (defonce repl (repl/connect "http://localhost:9000/repl")) ;; localhost for laptop, or an ipv4 address to repl against a mobile browser
     (bide/start! router {:default home :on-navigate #(swap! state assoc :page %) :html5? false})
     (reagent/render [root] (js/document.getElementById "app")))
 
