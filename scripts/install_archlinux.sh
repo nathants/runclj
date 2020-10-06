a47d92edbece815675a909d29ef2f68b8da5346d
nathants <me@nathants.com>
2020-10-06 14:02:16 -0700

    use npm server with cors

--- scripts/install_archlinux.sh
+++ scripts/install_archlinux.sh
@@ -4,6 +4,7 @@ sudo pacman -Sy --noconfirm \
      leiningen \
      npm
 cd /tmp
+npm -g install http-server
 rm -rf runclj
 git clone https://github.com/nathants/runclj
 sudo mv runclj/bin/* runclj/bin/.lein/ /usr/local/bin
