b3858acfffdd8d747c160634ab60ff127338b20e
nathants <me@nathants.com>
2021-11-12 21:19:54 -1000

    revert node server reload experiment

--- examples/server.cljs
+++ examples/server.cljs
@@ -1,14 +1,7 @@
 #!/usr/bin/env runclj
-^{:runclj {:npm [[express "4.17.1"]
-                 [ws "8.2.3"]]
+^{:runclj {:npm [[express "4.17.1"]]
            :deps []}}
-
 (ns server)
-
-(def express (js/require "express"))
-
-(defonce server-ref
-  (atom nil))
 
 (def port 8000)
 
@@ -17,46 +10,14 @@
   (.on res "finish" #(println (.-statusCode res) (.-url req)))
   (next))
 
-(defn router [server]
-  (-> server
-    (.get "/" (fn [req res]
-                (.send res "ok!")))
-    (.get "/nope" (fn [req res]
-                    (.send res "nope")))))
+(defn status-handler
+  [req res]
+  (.send res "ok 200"))
 
-(defn start []
-  (let [server (express)]
-    (.use server logging-middleware)
-    (router server)
-    (.listen server port)
-    (reset! server-ref server)))
-
-(defn ^:dev/after-load main []
+(defn main
+  []
   (println "listening on:" port)
-  (if-let [server @server-ref]
-    (.close server (fn [err] (start)))
-    (start)))
-
-;; #!/usr/bin/env runclj
-;; ^{:runclj {:npm [[express "4.17.1"]]
-;;            :deps []}}
-;; (ns server)
-
-;; (def port 8000)
-
-;; (defn logging-middleware
-;;   [req res next]
-;;   (.on res "finish" #(println (.-statusCode res) (.-url req)))
-;;   (next))
-
-;; (defn status-handler
-;;   [req res]
-;;   (.send res "ok 200"))
-
-;; (defn main
-;;   []
-;;   (println "listening on:" port)
-;;   (doto ((js/require "express"))
-;;     (.use logging-middleware)
-;;     (.get "/home" status-handler)
-;;     (.listen port)))
+  (doto ((js/require "express"))
+    (.use logging-middleware)
+    (.get "/home" status-handler)
+    (.listen port)))
